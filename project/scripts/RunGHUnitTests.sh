a6ee02cbde5343b9fec06a6c5a2714238f3921ff
Oliver Jones <oliver@deeperdesign.com>
2011-09-28 17:01:38 +1000

    Tweaked the test runner script to incorporate some stuff from Google's iPhone test runner script (see: http://code.google.com/p/google-toolbox-for-mac/source/browse/trunk/UnitTesting/RunIPhoneUnitTest.sh).  We now set up a home dir for the iPhone Simulator.

--- project/scripts/RunGHUnitTests.sh
+++ project/scripts/RunGHUnitTests.sh
@@ -5,9 +5,28 @@ if [ "$GHUNIT_CLI" = "" ] && [ "$GHUNIT_AUTORUN" = "" ]; then
   exit 0
 fi
 
+if [ "$PLATFORM_NAME" == "iphonesimulator" ]; then
+  # We kill the iPhone simulator because otherwise we run into issues where
+  # the unittests fail becuase the simulator is currently running, and
+  # at this time the iPhone SDK won't allow two simulators running at the same
+  # time.
+  set +e
+  /usr/bin/killall "iPhone Simulator"
+  set -e
+fi
+
 export DYLD_ROOT_PATH="$SDKROOT"
 export DYLD_FRAMEWORK_PATH="$CONFIGURATION_BUILD_DIR"
 export IPHONE_SIMULATOR_ROOT="$SDKROOT"
+export CFFIXED_USER_HOME="$TEMP_FILES_DIR/iPhoneSimulatorUserDir"
+
+# Cleanup user home directory
+if [ -d "$CFFIXED_USER_HOME" ]; then
+  rm -rf "$CFFIXED_USER_HOME"
+fi
+mkdir "$CFFIXED_USER_HOME"
+mkdir "$CFFIXED_USER_HOME/Documents"
+mkdir -p "$CFFIXED_USER_HOME/Library/Caches"
 
 export NSDebugEnabled=YES
 export NSZombieEnabled=YES
