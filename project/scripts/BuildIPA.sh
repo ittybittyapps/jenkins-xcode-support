dfa52e41f69e5064365ade3612ab8f18599d2914
Oliver Jones <oliver@deeperdesign.com>
2011-09-30 16:23:49 +1000

    Adds scripts for signing ipa binaries.

---
+++ project/scripts/BuildIPA.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+if [ ! -z "$1" -a -r $1 ]; then
+	
+	source $1 
+	
+	xcrun -sdk $SDK PackageApplication -verbose "$AppPath" -o "$IPAPath" \
+		--sign "$CodeSignIdent" --embed "$ProvisioningProfile"
+	
+else
+	echo "Usage: $0 [configfilepath]"
+	exit 1
+fi
---
+++ project/scripts/BuildPackagingScriptConfig.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+OUTPUT=$BUILT_PRODUCTS_DIR/BuildIPA.config
+
+rm -f $OUTPUT
+echo "SDK=$SDK" >> $OUTPUT
+echo "AppPath=\"$CODESIGNING_FOLDER_PATH\"" >> $OUTPUT
+echo "IPAPath=\"$SYMROOT/$TARGET_NAME.ipa\"" >> $OUTPUT
+echo "CodeSignIdent=\"$CODE_SIGN_IDENTITY\"" >> $OUTPUT
+echo "ProvisioningProfile=\"$PROVISIONING_PROFILE_DIR/$CONFIGURATION.mobileprovision\"" >> $OUTPUT
